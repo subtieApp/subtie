48f1f50310f2e32f679ce1900962584bb678e993
andts <and.tsarenko@gmail.com>
2018-06-10 19:39:28 +0300

    socket.io starting point

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', {hello: 'world'});
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
